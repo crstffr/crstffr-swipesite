510ca3ed87185e17018e4c49d112e4b6e91df541
crstffr <crstffr@gmail.com>
2012-08-10 16:22:58 -0500

    viewport meta tag not user scalable

--- public_html/index.php
+++ public_html/index.php
@@ -11,7 +11,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=1;" />
+    <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta names="apple-mobile-web-app-status-bar-style" content="black-translucent" />
 
@@ -55,8 +55,6 @@
     <script>
 
         $(function(){
-
-            alert($(window).width());
 
             $('.m-carousel').carousel({
                 dragRadius:10,
